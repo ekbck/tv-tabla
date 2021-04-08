6b8b2a1066c9d722f48fba6d8262271c40e847e6
Fabian Ekbäck <fabian.ekback@gmail.com>
2021-04-08 09:34:51 +0200

    Added menu icon and some Henning stuff

---
+++ src/components/Sidebar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Sidebar = () => {
+    return (
+        <div class="menu">
+            <a class="menu__link" href="">SVT 1</a>
+            <a class="menu__link" href="">SVT 2</a>
+            <a class="menu__link" href="">SVT Barn</a>
+            <a class="menu__link" href="">Kunskapskanalen</a>
+            <a class="menu__link" href="">SVT 24</a>
+        </div>
+    )
+}
+
+export default Sidebar;
