4509b3f5b43778460c9e78a3d4b245ae25d0b86d
Alexander Frödin <alexanderdelaval@gmail.com>
2021-04-08 15:11:17 +0200

    Added programList and container

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
-import Sidebar from './components/Sidebar'
+import Sidebar from './components/Sidebar';
+import ProgramList from './components/ProgramList';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+
+      <div>
         <Sidebar />
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
+
+      <div className="container">
+        <div className="row">
+
+          <div className="col-sm">
+
+          </div>
+
+          <div className="col-sm-6">
+            <ProgramList />
+          </div>
+
+          <div className="col-sm">
+
+          </div>
+
+        </div>
+      </div>
+
     </div>
   );
 }
